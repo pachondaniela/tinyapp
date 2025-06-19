831a6e4b63e1dfa18af00fcc0478e13411936c77
Daniela Pachon <[pachon.daniela@gmail.com]>
2025-06-19 08:10:59 -0400

    Include the urls_index ejs file to be able to render the data from the server into the front end using HTML

--- express_server.js
+++ express_server.js
@@ -6,15 +6,18 @@ const PORT = 8080; //Default por 8080
 app.set("view engine", "ejs");
 
 const urlDatabase = {
-   b2xVn2: "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+   b2xVn2:"http://www.lighthouselabs.ca",
+  "9sm5xK":"http://www.google.com"
 };
 
 app.get("/", (req, res) => {
   res.send("Hello!!");
 });
 
-//Add additional routers or endpoints:
+app.get("/urls", (req, res) => {
+  const templateVars = {urls: urlDatabase}
+  res.render("urls_index", templateVars);
+});
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
