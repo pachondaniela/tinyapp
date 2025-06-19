ca9bcf73c2d3bb7b8c153e21f43b0177bf2d6f20
Daniela Pachon <[pachon.daniela@gmail.com]>
2025-06-19 11:46:29 -0400

    Update express_server.js to store short-url and long-url key value pair to database when a POST request is received to /urls Update express_server.js to respond with a redirect from /urls to /urls/:id

--- express_server.js
+++ express_server.js
@@ -8,7 +8,7 @@ app.use(express.urlencoded({extended: true}))
 app.set("view engine", "ejs");
 
 function generateRandomString() {
-  return Math.random().toString(36).substring(2,6);
+  return Math.random().toString(36).substring(2,8);
 }
 
 const urlDatabase = {
@@ -26,9 +26,21 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);
-  res.send("Ok")
+  const randomID = generateRandomString()
+  const longURL = req.body.longURL
+  urlDatabase[randomID] = longURL
+  res.redirect(`/urls`)
 })
+
+app.get("/u/:id", (req, res) => {
+  const id = req.params.id
+  const longURL = urlDatabase[id]
+    if (!longURL) {
+    return res.status(404).send("URL not found");
+  }
+  res.redirect(longURL)
+})
+
 
 app.get("/urls/:id", (req, res) => {
   const id = req.params.id
@@ -44,3 +56,4 @@ app.get("/urls/:id", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
+
