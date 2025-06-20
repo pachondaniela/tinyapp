d13dd8ffb24801f734be4c985851b9a7212035a8
Daniela Pachon <[pachon.daniela@gmail.com]>
2025-06-19 21:40:39 -0400

    added the user database to make sure that it gets stored once a new user puts their information

--- express_server.js
+++ express_server.js
@@ -25,6 +25,23 @@ const urlDatabase = {
   "9sm5xK":"http://www.google.com"
 };
 
+
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
+};
+
+
+
+
 app.get("/urls", (req, res) => {
   const templateVars = {urls: urlDatabase , username: req.cookies["username"]};
   res.render("urls_index", templateVars);
@@ -49,6 +66,18 @@ app.post("/login", (req, res) => {
 app.post("/logout" , (req, res) => {
   res.clearCookie('username', { path: '/'}).redirect("/urls");
 });
+
+app.post("/register", (req, res) => {
+  const randomID = generateRandomString();
+  const email = req.body.email
+  const password = req.body.password
+  users[randomID] = {randomID, email, password}
+  res.cookie("NewID",randomID)
+  console.log(users)
+  res.redirect(`/urls`);
+});
+
+
 
 app.post("/urls", (req, res) => {
   const randomID = generateRandomString();
